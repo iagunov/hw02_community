9ec4aa33faba8865833eeecc81ab5f710395e399
Vasiliy Yagunov <vasiliy.yagunov@mail.ru>
2022-07-11 22:23:18 +0300

    add password reset

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 
 def index(request):
     post_list = Post.objects.all()
@@ -27,3 +32,27 @@ def group_posts(request, slug):
         'page_obj': page_obj
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = User.objects.get(username=username)
+    post_list = Post.objects.filter(author=author.id)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'page_obj': page_obj,
+        'number_post_list': post_list.count
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    num_posts = Post.objects.filter(author__username=post.author)
+    context = {
+        'post': post,
+        'num_posts': num_posts.count
+    }
+    return render(request, 'posts/post_detail.html', context)
